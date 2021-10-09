ff35525b16c9417afc6d8b004a118177450bfe0f
2231sshubham <2231sshubham@gmail.com>
2021-10-09 14:00:33 +0530

    c

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.get('*', function(req, res) {
 //
 // }
 
-app.listen(process.env.PORT || 0.0.0.0,function(){
+app.listen(process.env.PORT || 3001,function(){
   console.log(`Server started on port ${port}`);
 });
