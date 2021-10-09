8db2fa0a964324fff36d0b481c8f4c1f694c343b
2231sshubham <2231sshubham@gmail.com>
2021-10-09 14:09:28 +0530

    c

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ var url = "mongodb+srv://"+name+":"+pass+"@email-editor.kre0j.mongodb.net/email-
 mongoose.connect(url)
 
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://fierce-ocean-21330.herokuapp.com/']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://fierce-ocean-21330.herokuapp.com/']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request " + origin)
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable")
+//       callback(null, true)
+//     } else {
+//       console.log("Origin rejected")
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json());
 
 
